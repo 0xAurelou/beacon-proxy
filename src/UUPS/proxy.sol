359a8702a50e8e8eea58faf6d0d5f35975f13b76
0xAurelou <0xaurelou@gmail.com>
2024-01-18 16:02:00 +0100

    feat: base archi UUPS
    Signed-off-by: 0xAurelou <0xaurelou@gmail.com>

--- src/UUPS/proxy.sol
+++ src/UUPS/proxy.sol
@@ -2,11 +2,34 @@
 pragma solidity 0.8.19;
 
 contract PocProxy {
+    // keccak256(abi.encode(uint256(keccak256("PocProxy:Storage:V0")) - 1)) & ~bytes32(uint256(0xff))
     bytes32 public constant POC_PROXY_STORAGE_SLOT_V0 =
-        keccak256("poc.proxy.storage");
+        0x16c9df44581e907e6b2370ff2d47dd17c6199f47024750591f3709a54554d200;
 
     struct PocProxyStorage {
         address implementation;
         string version;
+    }
+
+    function getStorage() internal pure returns (PocProxyStorage storage s) {
+        assembly {
+            s.slot := POC_PROXY_STORAGE_SLOT_V0
+        }
+    }
+
+    function setImplementation(address implementation_) public {
+        getStorage().implementation = implementation_;
+    }
+
+    function setVersion() public {
+        getStorage().version = "V0";
+    }
+
+    function getImplementation() public view returns (address) {
+        return getStorage().implementation;
+    }
+
+    function getVersion() public view returns (string memory) {
+        return getStorage().version;
     }
 }
